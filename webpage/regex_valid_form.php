f1acf2a45f26c985afba0afd9d829e56db7d0fd3
Sarvar Abdullaev <sarrtv@gmail.com>
2019-03-07 12:42:37 +0500

    made regex corrections

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 	$pattern="";
 	$text="";
@@ -27,14 +27,14 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	<title>Valid Form</title>
 </head>
 <body>
-	<form action="valid_form.php" method="post">
+	<form action="regex_valid_form.php" method="post">
 		<dl>
 			<dt>Pattern</dt>
 			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
-			
+
 			<dt>Text</dt>
 			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
-			
+
 			<dt>Replace Text</dt>
 			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
 
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 			<dt>&nbsp;</dt>
 			<dd><input type="submit" value="Check"></dd>
 		</dl>
-		
+
 	</form>
 </body>
 </html>
